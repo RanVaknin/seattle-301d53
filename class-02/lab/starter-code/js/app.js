a44019798ff3cc3f45f3b4eb17b2e46a612a2bdd
RAN VAKNIN <ranvaknin0@gmail.com>
2019-10-31 11:45:41 -0700

    Working with Josh

--- class-02/lab/starter-code/js/app.js
+++ class-02/lab/starter-code/js/app.js
@@ -1,76 +1,81 @@
 
 
 // -------->     Wed OCT 30th RAN AND ELLEN     <--------
-
+let dataFiles = ['page-1.json', 'page-2.json']
 function Horn(horn) {
   this.image_url = horn.image_url;
   this.title = horn.title;
   this.description = horn.description;
   this.keyword = horn.keyword;
   this.horns = horn.horns;
+  this.page = 0
 }
+
+// function Horn (dataFiles) {
+//   for (let key in dataFiles) {
+//     this[key] = dataFiles[key];
+//   }
+// }
 
 Horn.allHorns = [];
 
 Horn.readJson = () => {
-  $.get('page-1.json')
-    .then(data => {
-      data.forEach(item =>{
-        Horn.allHorns.push(new Horn(item))
-      });
-    })
-    .then(Horn.loadHorns);
+  for (let i = 0; i < dataFiles.length; i++) {
+    $.get(dataFiles[i])
+      .then(data => {
+        data.forEach(item => {
+          let newHorn = new Horn(item);
+          newHorn.page = i;
+          Horn.allHorns.push(newHorn)
+          console.log('line 30');
+        });
+      }).then(Horn.loadHorns);
+    console.log('line 33');
+  }
 }
 
 // we loop over every object in json, every iteration 
+let hornHtml = [ $('#photo-template').html(), $('#photo-template2').html() ];
 
-Horn.prototype.render = function() {
+Horn.prototype.render = function () {
   $('main').append('<div class="clone"></div>');
   let hornClone = $('div[class="clone"]');
 
-  let hornHtml = $('#photo-template').html()
+  // let hornHtml = $('#photo-template').html()
+  // let hornHtml2 = $('#photo-template2').html()
+  // console.log('line 47');
 
-  hornClone.html(hornHtml);
+  //Render images based on number of hornHTML elements
+  for (let i = 0; i < hornHtml.length; i++) {
+    // console.log('line 49');
+    // console.log('this.page', this.page)
 
-  hornClone.find('h2').text(this.title);
-  hornClone.find('img').attr('src', this.image_url);
-  hornClone.find('p').text(this.description);
-  hornClone.removeClass();  
-  hornClone.addClass('hornImages');
-  hornClone.attr('data-value', this.keyword);
-
-  $('select').append(`<option value=${this.keyword}>${this.keyword}</option>`);
+    if (this.page === i) {
+      hornClone.html(hornHtml[i]);
+      // hornClone.html(hornHtml);
+      // console.log('line 60', 'page: ', this.page, ' ', i);
+      hornClone.find('h2').text(this.title);
+      hornClone.find('img').attr('src', this.image_url);
+      hornClone.find('p').text(this.description);
+      hornClone.removeClass();
+      hornClone.addClass('hornImages');
+      hornClone.attr('data-value', this.keyword);
+      $('select').append(`<option value=${this.keyword}>${this.keyword}</option>`);
+    }
+  }
 }
 
-$('select').on('change', function(){
+
+
+$('select').on('change', function () {
   let pickedThing = $(this).val()
   $('div').hide();
   $(`div[data-value=${pickedThing}`).show();
 })
 
-Horn.loadHorns = () =>{
+//Render all horns based on each horn image (1 per image)
+Horn.loadHorns = () => {
+  // for (let i = 0; i<Horn.allHorns)
   Horn.allHorns.forEach(horn => horn.render())
 }
 Horn.readJson();
-
-
-
-
-
-
-
-  // let sectionEl = document.getElementById("photo-template");
-  // let sectionEl = $('#photo-template')
-  // for(let i =0; i < Horn.allHorns.length ; i++){
-    // let imgEl = document.createElement('img')
-    // imgEl.src = Horn.allHorns[i].image_url
-    // sectionEl.appendChild(imgEl);
-    // sectionEl.append(`<img src=${Horn.allHorns[i].image_url}></img>`);
-    // sectionEl.append(`<p>${Horn.allHorns[i].title}</p>`);
-
-  
-  // let imgageSource = Horn.allHorns[0].image_url;
-  // $('img').attr('src', imgageSource);
-
-// Horn.readJson()
-// Horn.render()
